6e5b26de56cbed257d1a3378dbce5de85a9aed01
WallaceGaster <saitama.kurosakyy@gmail.com>
2025-06-01 12:49:20 -0600

    Agrega rutas CRUD para Colindancia: getById, postById, add y list por ID

--- routes/rutas.js
+++ routes/rutas.js
@@ -1,6 +1,14 @@
 const express = require('express');
 const router = express.Router();
 
+const userController = require('../controllers/userController');
+const colindanciaController = require('../controllers/colindanciaController');
+const inventarioController = require('../controllers/inventarioController');
+
+router.get('/getColindanciaById/:id',colindanciaController.getColindanciaById);
+router.post('/postColindanciaById',colindanciaController.postColindanciaById);
+router.post('/addColindancia',colindanciaController.addColindancia);
+router.get('/getColindanciasPorId/:id',colindanciaController.getColindanciasById);
 
 
 module.exports = router;
