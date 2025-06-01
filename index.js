4578401efbccda28c7c600f23f58b84b7dfedcce
WallaceGaster <saitama.kurosakyy@gmail.com>
2025-06-01 00:03:47 -0600

    Modelado de carpetas de toda la base de datos junto con la configuracion para pruebas de manera local y conexion a la base de datos

---
+++ index.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const conectarDB = require('./config/db');
+const app = express();
+const cors = require("cors");
+
+conectarDB();
+app.use(cors());
+app.use(express.json());
+
+const corsOptions = {
+  origin: 'http://localhost:4200', // Puerto donde corre Angular
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  allowedHeaders: ['Content-Type', 'Authorization']
+};
+app.use(cors(corsOptions));
+
+
+app.use('/casNov',require('./routes/rutas'));
+
+app.listen(5000,()=>{
+    console.log('Servidor corriendo perfectamente');
+})
