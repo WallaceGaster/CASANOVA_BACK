ef3066bbf0c7b9d6d31e9b6e2c501df464e30e67
WallaceGaster <saitama.kurosakyy@gmail.com>
2025-06-01 12:45:37 -0600

    Creación del modelo Colindancia con campos: id_inventario, manzanac, lotec, metros, direccion, nombreCalle

--- models/Colindancia.js
+++ models/Colindancia.js
@@ -1,1 +1,34 @@
-//--
+const mongoose = require('mongoose');
+
+
+const ColindanciaSchema = mongoose.Schema({
+    
+    id_inventario:{
+        type:String,
+        required:true
+    },
+    manzanac:{
+        type:String,
+        required:true
+    },
+    lotec:{
+        type:String,
+        requires:true
+    },
+    metros:{
+        type:String,
+        requires:true
+    },
+    direccion:{
+        type:String,
+        requires:true
+    },
+    nombreCalle:{
+        type:String,
+        requires:true
+    }
+    
+
+});
+
+module.exports = mongoose.model('Colindancia',ColindanciaSchema);
