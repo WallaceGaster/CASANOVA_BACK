17f4ea69dc7307299f4732552ada5e4fce917932
Bryan Morales <144483679+JalkemMorales@users.noreply.github.com>
2025-11-22 21:30:55 -0600

    Limite de archivos

--- index.js
+++ index.js
@@ -6,7 +6,8 @@ const cors = require("cors");
 
 conectarDB();
 app.use(cors());
-app.use(express.json());
+app.use(express.json({ limit: '50mb' }));
+app.use(express.urlencoded({ limit: '50mb', extended: true }));
 
 const corsOptions = {
   origin: 'http://localhost:4200', // Puerto donde corre Angular
